4f50ad24a64ac303372811a28f16b9f86b5fbf5e
tanetakumi <tanetakumi@gmail.com>
2022-11-18 16:28:06 +0900

    First commit

---
+++ src/main/java/tanetakumi/skinsmanager/SkinUploader.kt
@@ -0,0 +1,14 @@
+package tanetakumi.skinsmanager
+
+import net.dv8tion.jda.api.interactions.commands.Command
+
+class SkinUploader: Command() {
+    init {
+
+    }
+
+    override fun execute(event: CommandEvent) {
+
+    }
+
+}
---
+++ src/main/java/tanetakumi/skinsmanager/SkinsManager.kt
@@ -0,0 +1,16 @@
+package tanetakumi.skinsmanager
+
+import net.dv8tion.jda.api.JDABuilder
+import org.bukkit.plugin.java.JavaPlugin
+
+
+class SkinsManager : JavaPlugin() {
+    override fun onEnable() {
+        // Plugin startup logic
+        // Note: It is important to register your ReadyListener before building
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
